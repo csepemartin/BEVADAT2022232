6c04e29ff20ae625f52a009b782863b53c0e948f
csepemartin <csepemartin@stud.uni-obuda.hu>
2023-03-03 18:31:13 +0100

    [GYAK01 DONE]

---
+++ GYAK/GYAK01/GYAK01.py
@@ -0,0 +1,11 @@
+greeting = 'Hello World!'
+print(greeting)
+
+for letter in greeting:
+    print(letter)
+
+def printer(to_print):
+    for letter in to_print:
+        print(letter)
+
+printer(greeting)
