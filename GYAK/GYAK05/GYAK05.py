438d33c34d3425ba98d70cc8d5febba2b1cd9515
csepemartin <91947483+csepemartin@users.noreply.github.com>
2023-03-29 16:19:16 +0200

    GYAK05_1.0

---
+++ GYAK/GYAK05/GYAK05.py
@@ -0,0 +1,62 @@
+import numpy as np
+from scipy.stats import mode
+from typing import Tuple
+from sklearn.metrics import confusion_matrix
+import seaborn as sns
+
+
+class KNNClassifier:
+
+    def __init__(self,k:int,test_split_ratio:float):
+        self.k = k
+        self.test_split_ratio= test_split_ratio
+
+    @property
+    def k_neighbors(self):
+        return self.k
+
+    
+    @staticmethod
+    def load_csv(csv_path:str) -> Tuple[np.ndarray,np.ndarray]:
+        np.random.seed(42)
+        dataset = np.genfromtxt(csv_path,delimiter=',')
+        np.random.shuffle(dataset)
+        x,y =dataset[:,:-1],dataset[:,-1]
+        return x,y
+
+
+    def train_test_split(self,features:np.ndarray,
+                    lables:np.ndarray):
+    
+
+        test_size = int(len(features) * self.test_split_ratio)
+        train_size = len(features) - test_size
+        assert len(features) == test_size + train_size, "Size mismatch!"
+
+        x_train,y_train = features[:train_size,:],lables[:train_size]
+        x_test,y_test = features[train_size:,:],lables[train_size:]
+        self.x_train = x_train
+        self.y_train = y_train
+        self.x_test = x_test
+        self.y_test = y_test
+
+    def euclidean(self,element_of_x:np.ndarray) -> np.ndarray:
+        return np.sqrt(np.sum((self.x_train- element_of_x)**2,axis=0))
+    
+    def predict(self,x_test:np.ndarray):
+        labels_pred=[]
+        for x_test_element in x_test:
+            distances = self.euclidean(self.x_train,x_test_element)
+            distances = np.array(sorted(zip(distances,self.y_train)))
+            labels_pred = mode(distances[:self.k,1],keepdims = False).mode
+            labels_pred.append(labels_pred)
+        self.y_preds= labels_pred
+
+    def accuracy(self) -> float:
+        true_positive = (self.y_test == self.y_preds).sum()
+        return true_positive /len(self.y_test) * 100
+    
+
+    def confusion_matrix(self) -> None:
+        conf_matrix = confusion_matrix(self.y_test,self.y_preds)
+        return conf_matrix
