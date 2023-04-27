d98c4fcb1aebc00935f30fbd5068aed8c303d114
csepemartin <91947483+csepemartin@users.noreply.github.com>
2023-04-27 22:27:10 +0200

    HAZI09_1.0

---
+++ HAZI/HAZI09/HAZI09.py
@@ -0,0 +1,37 @@
+from sklearn.datasets import load_digits
+from sklearn.cluster import KMeans
+import numpy as np
+from scipy.stats import mode
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+
+
+class KMeansOnDigits():
+    
+
+    def __init__(n_clusters,random_state,self):
+        self.n_clusters= n_clusters
+        self.random_state = random_state
+
+    def load_dataset(self):
+        self.digits = load_digits()
+
+
+    def predict(self):
+        model = KMeans(n_clusters=self.n_clusters,random_state=self.random_state)
+        self.clusters = model.fit_predict(self.digits.data)
+
+    def get_labels(self):
+        result = np.full_like(self.clusters,'77')
+        for i in range(len(self.digits.target_names)):
+            each_cluster = np.where(self.clusters==i)
+            modes = mode(self.digits.target[each_cluster]).mode[0]
+            result[each_cluster] = modes
+        self.labels = result
+
+    def calc_accuracy(self):
+        return np.round(accuracy_score(self.digits.target,self.labels),2)
+    
+    def confusion_matrix(self):
+        self.mat = confusion_matrix()
+        
